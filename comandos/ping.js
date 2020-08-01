a2eae10dc69617cbe6a785d9bf4535984ef62595
Monoverde888 <minecraftersov@gmail.com>
2020-07-31 20:11:33 -0400

    Inicializado

---
+++ comandos/ping.js
@@ -0,0 +1,6 @@
+//Esta función recibe el parametro client, message, y args para ser usados:
+module.exports = (client, message, args) => { 
+    message.channel.send("Pong!"); 
+  
+  }
+  
---
+++ config.js
@@ -0,0 +1,10 @@
+let config = {
+    token: "Token de tu bot", // Reemplaze esto con el token de tu de bot.
+    prefix: "Prefijo", // El prefix de su bot para indentificar sus comandos.
+  
+  }
+  
+  // Aqui exportamos el objeto config:
+  module.exports = config;
+  
+  
